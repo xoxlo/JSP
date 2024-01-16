32e0b1a007d6449226116a1d723aff1b9d7de907
ten4roid <thdckdtjr8@gmail.com>
2024-01-16 21:30:15 +0900

    gimotti

---
+++ day12/src/com/jspex/beans/MemberVO.java
@@ -0,0 +1,32 @@
+package com.jspex.beans;
+
+public class MemberVO {
+	private int num;
+	private String name;
+	private String birthday;
+	
+	public MemberVO() {;}
+
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	
+	
+	
+	
+}
